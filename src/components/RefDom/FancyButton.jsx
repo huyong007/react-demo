b8ca99e102f9ca2a2c0ffcbd1eaf3cfda15028f3
yong.hu <yong.hu@horizon.ai>
2021-11-29 09:48:40 +0800

    startLearnRef

---
+++ src/components/RefDom/FancyButton.jsx
@@ -0,0 +1,17 @@
+function FancyButton(props) {
+    return (
+        <button className="FancyButton">
+            {props.children}
+        </button>
+    )
+}
+
+const FancyButton = React.forwardRef((props,ref)=>(
+    <button ref={ref} className="FancyButton">
+        {props.children}
+    </button>
+));
+
+const ref = React.createRef();
+<FancyButton ref={ref}>Click me!</FancyButton>;
+
